959301c58586bbd373b6e6b2ca800cf1906e7992
babblebabel <the21phoenix@gmail.com>
2020-07-17 11:21:37 +0530

    Additional changes to Routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,9 +1,9 @@
 //Importing the express server
 const express = require('express')
-const author = require('../models/author')
 //Creating of a router 
 const router = express.Router()
 const Author = require('../models/author')
+const author = require('../models/author')
 
 //All authors route
 router.get('/',(req,res)=>{
@@ -12,12 +12,39 @@ router.get('/',(req,res)=>{
 
 //New author route 
 router.get('/new',(req,res)=>{
-    res.render('authors/new', {author: new author()})
+    res.render('authors/new', {author: new Author()})
 })
 
 //Create author route 
-router.post('/',(req,res)=>{
-    res.send('Create')
+router.post('/',async (req,res)=> {
+    
+    const author = new Author({
+       name: req.body.name
+   })
+
+   try{
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+   } catch {
+           res.render('authors/new', { 
+               author : author,
+               errorMessage: 'Error creating Author' 
+
+           })
+   }
+
+author.save((err,newAuthor)=>{
+    if(err) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    } else{
+        
+    }
+})
+
 })
 
 
