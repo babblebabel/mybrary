df39f110674a177672a72a57da8d947d06fd8d42
babblebabel <the21phoenix@gmail.com>
2020-07-18 11:18:54 +0530

    Updated the author route

--- routes/authors.js
+++ routes/authors.js
@@ -2,12 +2,33 @@
 const express = require('express')
 //Creating of a router 
 const router = express.Router()
+//Importing the Author Model
 const Author = require('../models/author')
 const author = require('../models/author')
 
 //All authors route
-router.get('/',(req,res)=>{
-    res.render('authors/index')
+router.get('/',async (req,res)=>{
+    //console.log(`Checking type of response ${typeof res}`)
+    let search = {}
+     if(req.query.name!==null && req.query.name!==''){
+         search.name= new RegExp(req.query.name, 'i')
+         console.log(typeof search)
+         console.log(req.query)
+         console.log(search.name)
+     }
+    try{
+        const authors = await Author.find({search})
+        res.render('../views/authors/index.ejs', {
+            authors:authors,
+        //    search:req.query
+        })
+   } catch{
+        res.redirect('/')
+   }
+   
+   
+   
+    //res.render('authors/index')
 })
 
 //New author route 
